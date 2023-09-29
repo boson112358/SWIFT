1d51a3911d35cf36f5ef061b2732bdb49b56717e
Nikyta Shchutskyi <shchutskyi@bega.lorentz.leidenuniv.nl>
2023-09-29 13:32:14 +0200

    changedIC

--- examples/MHDTests/RobertsFlow/makeICnew.py
+++ examples/MHDTests/RobertsFlow/makeICnew.py
@@ -43,12 +43,21 @@ ids = np.linspace(1, N, N)
 m = np.ones(N) * rho * vol / N
 u = np.ones(N) * u0
 
+
 v[:, 0] = V0 * np.sin(k * pos[:, 1]) * np.cos(k * pos[:, 0])
 v[:, 1] = -V0 * np.sin(k * pos[:, 0]) * np.cos(k * pos[:, 1])
 v[:, 2] = V0 * np.cos(k * pos[:, 1]) * np.cos(k * pos[:, 0]) * np.sqrt(2)
 
-B[:, 0] = B0 * np.cos(k * pos[:, 2])
-B[:, 1] = B0 * np.sin(k * pos[:, 2])
+#B[:, 0] = B0 * np.cos(k * pos[:, 2])
+#B[:, 1] = B0 * np.sin(k * pos[:, 2])
+#####//////
+A0=B0/k
+B[:, 0] = B0 * (np.sin(k * pos[:,2]) +  np.cos(k * pos[:,1]))
+B[:, 1] = B0 * (np.sin(k * pos[:,0]) +  np.cos(k * pos[:,2]))
+B[:, 2] = B0 * (np.sin(k * pos[:,1]) +  np.cos(k * pos[:,0]))
+A[:, 0] = A0 * (np.sin(k * pos[:,2]) +  np.cos(k * pos[:,1]))
+A[:, 1] = A0 * (np.sin(k * pos[:,0]) +  np.cos(k * pos[:,2]))
+A[:, 2] = A0 * (np.sin(k * pos[:,1]) +  np.cos(k * pos[:,0]))
 
 ###---------------------------###
 
@@ -84,5 +93,6 @@ grp.create_dataset("SmoothingLength", data=h, dtype="f")
 grp.create_dataset("InternalEnergy", data=u, dtype="f")
 grp.create_dataset("ParticleIDs", data=ids, dtype="L")
 grp.create_dataset("MagneticFluxDensity", data=B, dtype="f")
+grp.create_dataset("",data=A, dtype="f")
 
 fileOutput.close()
